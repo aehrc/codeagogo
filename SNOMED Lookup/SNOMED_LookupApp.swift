7ba2c29c910769d060d8e794ce827ed7e8a8591f
Dion McMurtrie <dion.mcmurtrie@csiro.au>
2026-01-13 17:53:44 +1000

    Initial Commit

---
+++ SNOMED Lookup/SNOMED_LookupApp.swift
@@ -0,0 +1,17 @@
+//
+//  SNOMED_LookupApp.swift
+//  SNOMED Lookup
+//
+//  Created by Dion McMurtrie on 13/1/2026.
+//
+
+import SwiftUI
+
+@main
+struct SNOMED_LookupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
